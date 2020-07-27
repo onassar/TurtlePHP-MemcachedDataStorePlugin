e356dba11136e8fe45f2faea4a0169840ac86bf1
onassar <onassar@gmail.com>
2020-07-27 12:17:00 -0400

    Default config file cleanup

--- config.default.inc.php
+++ config.default.inc.php
@@ -7,44 +7,91 @@
     namespace Plugin\MemcachedDataStore;
 
     /**
-     * Data
+     * Plugin Config Data
      * 
      */
 
-    // Whether duration should be benchmarked
+    /**
+     * $benchmark
+     * 
+     * Whether memcached requests should be benchmarked for performance.
+     * 
+     * @var     bool (default: false)
+     * @access  private
+     */
     $benchmark = false;
 
-    // Key, which when passed in _GET, which flush the data store
-    $flushKey = 'flush';
-
-    // Whether or not to allow the data store to be flushed
-    $flushing = true;
-
-    // Key, which when passed in _GET, bypasses the cache
+    /**
+     * $flushing
+     * 
+     * Key, which when passed in via $_GET, causes the cache to be bypassed (for
+     * that request).
+     * 
+     * @var     string (default: 'bypass')
+     * @access  private
+     */
     $bypassKey = 'bypass';
 
-    // Namespace for ensuring no key-conflicts
+    /**
+     * $flushing
+     * 
+     * Whether or not to allow the data store to be flushed via a $_GET request.
+     * 
+     * @var     bool (default: true)
+     * @access  private
+     */
+    $flushing = true;
+
+    /**
+     * $flushKey
+     * 
+     * Key, which when passed in via a $_GET parameter, will cause the data
+     * store to be flushed.
+     * 
+     * @var     string (default: 'flush')
+     * @access  private
+     */
+    $flushKey = 'flush';
+
+    /**
+     * $namespace
+     * 
+     * Used to ensure no key-conflicts.
+     * 
+     * @var     string (default: 'application.local')
+     * @access  private
+     */
     $namespace = 'application.local';
 
-    // Server addresses and ports
+    /**
+     * $servers
+     * 
+     * Specific the servers and ports that should be used for storage.
+     * 
+     * @var     array
+     * @access  private
+     */
+    $servers = array();
+    $server = array('127.0.0.1', '11211');
+    array_push($servers, $server);, var)
     $servers = array(
         array('127.0.0.1', '11211')
     );
 
     /**
-     * Config storage
+     * $pluginConfigData
+     * 
+     * @var     array
+     * @access  private
+     */
+    $pluginConfigData = compact(
+        'benchmark', 'bypassKey', 'flushing', 'flushKey', 'namespace',
+        'servers'
+    );
+
+    /**
+     * Storage
      * 
      */
-
-    // Store
-    \Plugin\Config::add(
-        'TurtlePHP-MemcachedDataStorePlugin',
-        array(
-            'benchmark' => $benchmark,
-            'flushKey' => $flushKey,
-            'flushing' => $flushing,
-            'bypassKey' => $bypassKey,
-            'namespace' => $namespace,
-            'servers' => $servers
-        )
-    );
+    $key = 'TurtlePHP-MemcachedDataStorePlugin';
+    \Plugin\Config::add($key, $pluginConfigData);
